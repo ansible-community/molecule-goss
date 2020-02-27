b4a51038ff00e70b637d9872c25625fcbf96de35
Sorin Sbarnea <ssbarnea@redhat.com>
2020-02-27 18:24:43 +0000

    Migrate goss plugin to v3
    - fixed links
    - adopted standard cookiecutter location
    - copied boilerplate from v3 plugin

--- molecule_goss/test/unit/test_goss_unit.py
+++ molecule_goss/test/unit/test_goss_unit.py
@@ -18,40 +18,31 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-import os
-
 import pytest
 
 from molecule import config
 from molecule_goss import goss
-from molecule.verifier.lint import yamllint
 
 
 @pytest.fixture
 def _patched_ansible_verify(mocker):
-    m = mocker.patch('molecule.provisioner.ansible.Ansible.verify')
-    m.return_value = 'patched-ansible-verify-stdout'
+    m = mocker.patch("molecule.provisioner.ansible.Ansible.verify")
+    m.return_value = "patched-ansible-verify-stdout"
 
     return m
 
 
 @pytest.fixture
 def _patched_goss_get_tests(mocker):
-    m = mocker.patch('molecule_goss.goss.Goss._get_tests')
-    m.return_value = ['foo.py', 'bar.py']
+    m = mocker.patch("molecule_goss.goss.Goss._get_tests")
+    m.return_value = ["foo.py", "bar.py"]
 
     return m
 
 
 @pytest.fixture
 def _verifier_section_data():
-    return {
-        'verifier': {
-            'name': 'goss',
-            'env': {'FOO': 'bar'},
-            'lint': {'name': 'yamllint'},
-        }
-    }
+    return {"verifier": {"name": "goss", "env": {"FOO": "bar"}}}
 
 
 # NOTE(retr0h): The use of the `patched_config_validate` fixture, disables
@@ -71,46 +62,35 @@ def test_default_options_property(_instance):
 
 
 def test_default_env_property(_instance):
-    assert 'MOLECULE_FILE' in _instance.default_env
-    assert 'MOLECULE_INVENTORY_FILE' in _instance.default_env
-    assert 'MOLECULE_SCENARIO_DIRECTORY' in _instance.default_env
-    assert 'MOLECULE_INSTANCE_CONFIG' in _instance.default_env
+    assert "MOLECULE_FILE" in _instance.default_env
+    assert "MOLECULE_INVENTORY_FILE" in _instance.default_env
+    assert "MOLECULE_SCENARIO_DIRECTORY" in _instance.default_env
+    assert "MOLECULE_INSTANCE_CONFIG" in _instance.default_env
 
 
-@pytest.mark.parametrize('config_instance', ['_verifier_section_data'], indirect=True)
+@pytest.mark.parametrize("config_instance", ["_verifier_section_data"], indirect=True)
 def test_env_property(_instance):
-    assert 'bar' == _instance.env['FOO']
-
-
-@pytest.mark.parametrize('config_instance', ['_verifier_section_data'], indirect=True)
-def test_lint_property(_instance):
-    assert isinstance(_instance.lint, yamllint.Yamllint)
+    assert "bar" == _instance.env["FOO"]
 
 
 def test_name_property(_instance):
-    assert 'goss' == _instance.name
+    assert "goss" == _instance.name
 
 
 def test_enabled_property(_instance):
     assert _instance.enabled
 
 
-def test_directory_property(_instance):
-    parts = _instance.directory.split(os.path.sep)
-
-    assert 'tests' == parts[-1]
-
-
-@pytest.mark.parametrize('config_instance', ['_verifier_section_data'], indirect=True)
+@pytest.mark.parametrize("config_instance", ["_verifier_section_data"], indirect=True)
 def test_options_property(_instance):
     x = {}
 
     assert x == _instance.options
 
 
-@pytest.mark.parametrize('config_instance', ['_verifier_section_data'], indirect=True)
+@pytest.mark.parametrize("config_instance", ["_verifier_section_data"], indirect=True)
 def test_options_property_handles_cli_args(_instance):
-    _instance._config.args = {'debug': True}
+    _instance._config.args = {"debug": True}
     x = {}
 
     # Does nothing.  The `goss` command does not support
@@ -133,34 +113,11 @@ def test_execute(
 
     _patched_ansible_verify.assert_called_once_with()
 
-    msg = 'Executing Goss tests found in {}/...'.format(_instance.directory)
+    msg = "Executing Goss tests found in {}/...".format(_instance.directory)
     patched_logger_info.assert_called_once_with(msg)
 
-    msg = 'Verifier completed successfully.'
+    msg = "Verifier completed successfully."
     patched_logger_success.assert_called_once_with(msg)
-
-
-def test_execute_does_not_execute(
-    patched_ansible_converge, patched_logger_warn, _instance
-):
-    _instance._config.config['verifier']['enabled'] = False
-    _instance.execute()
-
-    assert not patched_ansible_converge.called
-
-    msg = 'Skipping, verifier is disabled.'
-    patched_logger_warn.assert_called_once_with(msg)
-
-
-def test_does_not_execute_without_tests(
-    patched_ansible_converge, patched_logger_warn, _instance
-):
-    _instance.execute()
-
-    assert not patched_ansible_converge.called
-
-    msg = 'Skipping, no tests found.'
-    patched_logger_warn.assert_called_once_with(msg)
 
 
 def test_execute_bakes():
